11c458fe2b72c307dfdd6b6670ae999049cbe3a4
omkar9552 <80102635+omkar9552@users.noreply.github.com>
2022-11-22 11:43:24 +0530

    Update 272Omkar_Shinde_CV2_Assignment2.py

--- 272Omkar_Shinde_CV2_Assignment2.py
+++ 272Omkar_Shinde_CV2_Assignment2.py
@@ -1,54 +1,86 @@
+import streamlit as st
 import cv2
-top_left_corner=[]
-bottom_right_corner=[]
-image = cv2.imread('jellyfish.jpg')
-def drawRectangle(action, x, y, flags, *userdata):
-  # Referencing global variables
-  global top_left_corner, bottom_right_corner
-  # Mark the top left corner when left mouse button is pressed
-  if action == cv2.EVENT_LBUTTONDOWN:
-    top_left_corner = [(x,y)]
-    # When left mouse button is released, mark bottom right corner
-  elif action == cv2.EVENT_LBUTTONUP:
-    bottom_right_corner = [(x,y)]   
-    # Draw the rectangle
-    cv2.rectangle(image, top_left_corner[0], bottom_right_corner[0], (0,255,0),2, 8)
-    cv2.imshow("Windo",image)
-# cv_imshow(image)
-# Make a temporary image, will be useful to clear the drawing
-temp = image.copy()
-# Create a named window
-cv2.namedWindow("Win")
-# highgui function called when mouse events occur
-cv2.setMouseCallback('Win',drawRectangle)
-k=0
-while k!=113:
-  cv2.imshow("Win", image)
-  k = cv2.waitKey(0)
-  if (k == 99):
-    image= temp.copy()
-    cv2.imshow("Win", image)
-cv2.destroyAllWindows()
+from PIL import Image
+import numpy as np
+
+def filter():
+        wood = st.file_uploader("Upload Your Image For Filter", type=['jpg', 'png', 'jpeg'])
+        salt = st.file_uploader("Upload Your Image To Remove salt and pepper", type=['jpg', 'png', 'jpeg'])
+        if not wood or not salt:
+               return None
 
 
-maxScaleUp = 100
-scaleFactor = 1
-windowName = "Resize Image"
-trackbarValue = "Scale"
-cv2.namedWindow(windowName, cv2.WINDOW_AUTOSIZE)
+        original_image_wood = Image.open(wood)
+        original_image_wood = np.array(original_image_wood)
 
+        st.image(original_image_wood, caption="★ Original Image of Wood★")
 
-def scaleImage(*args):
-    # Get the scale factor from the trackbar
-    scaleFactor = 1+ args[0]/100.0
-    # Resize the image
-    scaledImage = cv2.resize(image, None, fx=scaleFactor, fy = scaleFactor, interpolation = cv2.INTER_LINEAR)
-    cv2.imshow(windowName, scaledImage)
+        st.text("______________________________________________________________________________________________")
+        original_image_salt = Image.open(salt)
+        original_image_salt = np.array(original_image_salt)
 
+        st.image(original_image_salt, caption="★ Original Image of salt and pepper★")
+        filter = st.radio(
+                "➳ Choose your Favourite Filter 👇",
+                ["twoconvkernel", "average_blurring", "guassian_blur", "median_blur","bilateral_blur","salt_pepper_removal"],
+                key="filter"
+            )
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>',
+             unsafe_allow_html=True)
+        if filter == "twoconvkernel":
+                twoconvkernel(original_image_wood)
+        elif filter == "average_blurring":
+                average_blurring(original_image_wood)
+        elif filter == "guassian_blur":
+                guassian_blur(original_image_wood)
+        elif filter == "median_blur":
+                median_blur(original_image_wood)
+        elif filter == "salt_pepper_removal":
+                salt_pepper_removal(original_image_salt)
+        elif filter == "bilateral_blur":
+                bilateral_blur(original_image_wood)       
+        else:
+                st.text("Sorry Filter is not Available")
 
-# Create trackbar and associate a callback function
-cv2.createTrackbar(trackbarValue, windowName, scaleFactor, maxScaleUp, scaleImage)
-# Display the image
-cv2.imshow(windowName, image)
-c = cv2.waitKey(0)
-cv2.destroyAllWindows()
+        
+def twoconvkernel(original_image_wood):
+    kernel2 = np.ones((5, 5), np.float32) / 25
+    img = cv2.filter2D(src=original_image_wood, ddepth=-1, kernel=kernel2)
+    label = "✵ output image✵" 
+    st.image(img, caption=label)
+
+def average_blurring(original_image_wood):
+    img_blur = cv2.blur(src=original_image_wood, ksize=(3,3)) # Using the blur function to blur an image where ksize is the kernel size
+    label = "✵ output image✵" 
+    st.image(img_blur, caption=label)
+    
+def guassian_blur(original_image_wood):
+    gau_blur = cv2.GaussianBlur(src=original_image_wood, ksize=(5,5),sigmaX=0, sigmaY=0)
+    label = "✵ output image✵" 
+    st.image(gau_blur, caption=label)
+
+def median_blur(original_image_wood):
+    median = cv2.medianBlur(src=original_image_wood, ksize=5)
+    label = "✵ output image✵" 
+    st.image(median, caption=label)
+
+def salt_pepper_removal(original_image_salt):
+    median = cv2.medianBlur(src=original_image_salt, ksize=3)
+    label = "✵ output image✵" 
+    st.image(median, caption=label)
+
+def bilateral_blur(original_image_wood):
+    bilateral_filter = cv2.bilateralFilter(src=original_image_wood, d=9, sigmaColor=75, sigmaSpace=75)
+    label = "✵ output image✵" 
+    st.image(bilateral_filter, caption=label)
+    
+def main_opration():
+    st.title("Blurring and salt pepper noise removal")
+   
+
+    filter()
+    st.text("_____________________________________________________________________________________________________________")
+
+if __name__ == "__main__":
+    main_opration()
+    
